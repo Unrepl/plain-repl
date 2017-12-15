22e7d0f5b5f82653ecd8ce8bfc073433164da589
Christophe Grand <christophe@cgrand.net>
2017-12-15 13:23:57 +0100

    new, more minimal, upgrade strategy

---
+++ src/plain_repl/min.cljs
@@ -0,0 +1,24 @@
+(ns plain-repl.min)
+
+;; ideally this namespace content should make its way to cljs itself
+
+(defprotocol AsyncReader
+  "Asynchronous stream of strings."
+  (read-chars [r f] "Calls f with a string or nil (EOF)")
+  (pushback [r s] "Unread s"))
+
+(deftype Suspension [f])
+
+(defn suspension? [x] (instance? Suspension x))
+
+(defn suspend-repl
+  "Returns a repl suspension. 
+   When an eval returns a suspension then the repl must yield control to the suspension and not print or loop until the suspension completes."
+  [f]
+  (Suspension. f))
+
+(defn yield-control
+  "Yields control to the suspension, passing it input and output.
+   When the suspension completes it calls resume-repl (a function of two arguments: value and exception)."
+  [suspension async-reader print-fn resume-repl]
+  ((.-f suspension) async-reader print-fn resume-repl))
